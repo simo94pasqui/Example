591c6a0f0987e7269770012ab790837a8f6c74f3
simo94pasqui <simo94.pasqualini@gmail.com>
2020-04-19 21:56:41 +0200

    aggiunta cache android

--- ExampleAndroid/Iliad/app/src/main/java/com/example/iliad/MainActivity.java
+++ ExampleAndroid/Iliad/app/src/main/java/com/example/iliad/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.iliad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.webkit.WebChromeClient;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,11 +21,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        webb = (WebView)findViewById(R.id.webPage);
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading Data...");
+        progressDialog.setCancelable(false);
+        webb = (WebView) findViewById(R.id.webPage);
         WebSettings webs = webb.getSettings();
         webs.setJavaScriptEnabled(true);
+        webb.getSettings().setAppCachePath(getApplicationContext().getFilesDir().getAbsolutePath() + "/cache");
+        webb.getSettings().setDatabasePath(getApplicationContext().getFilesDir().getAbsolutePath() + "/databases");
         webb.loadUrl("https://www.iliad.it/account/");
-        webb.setWebViewClient(new WebViewClient());
+        webb.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        webb.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress) {
+                if (progress < 100)
+                    progressDialog.show();
+                if (progress == 100)
+                    progressDialog.dismiss();
+            }
+        });
+
+        webb.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction()
+                        == MotionEvent.ACTION_UP && webb.canGoBack()) {
+                    //handler.sendEmptyMessage(1);
+                    webb.goBack();
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
     @Override
